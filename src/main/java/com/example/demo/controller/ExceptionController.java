3f1eb13622d92435f9cfe3c9d8dfd7a8ae3d9402
Sergey Vakulenko <svak@MacBook-Pro-Sergey.local>
2022-05-08 23:42:58 +0300

    add on erro on UI

---
+++ src/main/java/com/example/demo/controller/ExceptionController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handle(final MethodArgumentNotValidException ex) {
+        final BindingResult bindingResult = ex.getBindingResult();
+        final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        final List<String> errors = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest()
+                .body(errors);
+    }
+
+}
